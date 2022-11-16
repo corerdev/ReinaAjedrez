991bbefbb76f882cc5cddc944093d799f7574f62
Core <Core@LAPTOP-K19JUHSA>
2022-11-16 19:18:00 +0100

    Crea los métodos get y set para los atributos.
    Recuerda que para el método set se debe tener en cuenta que los valores
    indicados sean correctos (las filas van del 1 al 8 -ambos inclusive- y
    las columnas de la 'a' a la 'h' -ambas inclusive-) y si no se lance una
    excepción del tipo IllegalArgumentException con el mensaje adecuado. En
    caso de que la posición no sea la correcta no deben modificarse los
    atributos. Realiza un commit

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
@@ -1,21 +1,20 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public enum Color {
-		BLANCO("Blanco"),
-		NEGRO("Negro");
-	
+	BLANCO("Blanco"), NEGRO("Negro");
+
 	public String cadenaAMostrar;
 
 	private Color(String cadenacolor) {
-		
-		this.cadenaAMostrar = cadenacolor;		
+
+		this.cadenaAMostrar = cadenacolor;
 	}
-	
+
 	@Override
 	public String toString() {
-		
+
 		return cadenaAMostrar;
-		
+
 	}
 
 }
--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -3,9 +3,33 @@ package org.iesalandalus.programacion.reinaajedrez.modelo;
 public class Posicion {
 
 	private int fila;
-	
+
 	private char columna;
-	
-	
-	
+
+	public int getFila() {
+		return fila;
+	}
+
+	private void setFila(int fila) {
+		if (fila < 1 || fila > 8) {
+			throw new IllegalArgumentException("El valor de la fila es incorrecto.");
+
+		} else {
+			this.fila = fila;
+		}
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	private void setColumna(char columna) {
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException("El valor de la columna es incorrecto.");
+
+		} else {
+			this.columna = columna;
+		}
+	}
+
 }
