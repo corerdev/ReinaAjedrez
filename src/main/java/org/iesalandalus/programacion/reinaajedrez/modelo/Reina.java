4f68450f561344abfbccad756f0283d51006accd
Core <Core@LAPTOP-K19JUHSA>
2022-11-16 23:58:03 +0100

    Crea un constructor por defecto para esta clase que cree una reina blanca en la posición '1d'. Realiza un commit.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -4,6 +4,39 @@ public class Reina {
 
 	private Color color;
 	private Posicion posicion;
-	
-	
+
+	public Color getColor() {
+		return color;
+	}
+
+	private void setColor(Color color) {
+		if (color.equals(Color.NEGRO) || color.equals(Color.BLANCO)) {
+
+			this.color = color;
+
+		} else {
+
+			throw new IllegalArgumentException("ERROR: Color no válido.");
+		}
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	private void setPosicion(Posicion posicion) {
+
+		if (posicion.getFila() >= 1 && posicion.getFila() <= 8) {
+			if (posicion.getColumna() >= 'a' && posicion.getColumna() <= 'H') {
+				this.posicion = posicion;
+			} else {
+				throw new IllegalArgumentException("ERROR: Columna no válida.");
+			}
+		} else {
+
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		}
+
+	}
+
 }
