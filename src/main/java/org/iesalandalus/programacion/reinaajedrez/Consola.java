34537be7a6fb6e1fb56b62fe1e7b07701ef68036
Core <Core@LAPTOP-K19JUHSA>
2022-11-17 02:28:54 +0100

    Crea el método elegirOpcionMenu que mostrará un mensaje para que elijamos una opción del menú anteriormente creado y nos pedirá que introduzcamos por teclado la opción hasta que ésta sea valida. Devolverá la opción elegida. Realiza un commit.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
 
 	private Consola() {
@@ -17,6 +19,30 @@ public class Consola {
 		System.out.println("3 - Mover la reina.");
 		System.out.println("4 - Salir.");
 
+	}
+	
+	
+	public static void elegirOpcionMenu() {
+		
+		
+		int seleccion;
+		
+		do {
+			
+			System.out.println("Por favor, elija una de las opciones del menú: ");
+			Entrada.entero(seleccion);
+		} while (seleccion <1 || seleccion > 4);
+		
+		if (seleccion == 1) {
+			System.out.println("Ha elegido crear la reina por defecto. Esta será blanca, y empezará en 1d.");
+		} if (selección == 2) {
+			System.out.println("Ha elegido crear una reina de color determinado.");
+			} if (seleccion == 3) {
+				System.out.println("Ha elegido mover a la reina.");
+				} if (seleccion == 4) {
+					System.out.println("Ha elegido cerrar el programa.");
+				}
+			
 	}
 	
 }
