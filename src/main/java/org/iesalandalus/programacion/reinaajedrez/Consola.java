0e31fdf7a20ab8a6e668c5900f39ae24b1502bf7
Core <Core@LAPTOP-K19JUHSA>
2022-11-17 02:19:04 +0100

    Crea el método mostrarMenu que mostrará el menú con las opciones de nuestra aplicación: crear reina por defecto, crear reina eligiendo el color, mover y salir. Realiza un commit.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -7,5 +7,16 @@ public class Consola {
 	}
 	
 	
+	public static void mostrarMenu() {
+		
+		System.out.println("--------------------------------");
+		System.out.println("Simulador de reina de ajedrez");
+		System.out.println("Las opciones disponibles son:");
+		System.out.println("1 - Crear reina por defecto.");
+		System.out.println("2 - Crear reina eligiendo el color.");
+		System.out.println("3 - Mover la reina.");
+		System.out.println("4 - Salir.");
+
+	}
 	
 }
