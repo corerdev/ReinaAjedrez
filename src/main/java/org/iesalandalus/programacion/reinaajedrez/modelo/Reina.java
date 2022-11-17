c96dbc543ae3ae98570841292aa674ee2d17cb34
Core <Core@LAPTOP-K19JUHSA>
2022-11-17 01:56:44 +0100

    Crea el método mover que acepte como parámetro una Direccion y la cantidad de pasos a mover en dicha dirección. Los pasos deben estar comprendidos entre 1 y 7 (ambos inclusive) y la dirección no puede ser nula o de lo contrario debe lanzar una excepción adecuada (NullPointerException o IllegalArgumentException) con el mensaje adecuado. Si no puede realizar dicho movimiento, debido a que la reina se sale del tablero, se debe lanzar una excepción del tipo OperationNotSupportedException con un mensaje adecuado y no modificará la posición de la reina. Realiza un commit.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Reina {
 
 	private Color color;
@@ -47,15 +49,78 @@ public class Reina {
 	}
 
 	public Reina(Color color) {
-		if (color.equals(Color.NEGRO)) {
-			color = Color.NEGRO;
-			posicion = new Posicion(8, 'b');
+		if (color == null) {
+			throw new NullPointerException("ERROR: El color no puede ser nulo.");
 		} else {
-			color = Color.BLANCO;
+		if (color.equals(Color.NEGRO)) {
+			this.setColor(Color.NEGRO);
+			posicion = new Posicion(8, 'd');
+		} else {
+			this.setColor(Color.BLANCO);
 			posicion = new Posicion(1, 'd');
+		}
 		}
 
 	}
 
+	public void mover(Direccion direccion, int pasos)
+			throws OperationNotSupportedException, IllegalArgumentException, NullPointerException {
 
+		if (direccion == null) {
+			throw new NullPointerException("ERROR: La dirección no puede ser nula.");
+		}
+		if (pasos < 1 || pasos > 7) {
+			throw new IllegalArgumentException("ERROR: El número de pasos debe estar comprendido entre 1 y 7.");
+		}
+
+		int filaTemporal = posicion.getFila();
+		char columnaTemporal = posicion.getColumna();
+		Posicion posicionTemporal = null;
+		switch (direccion) {
+		case NORTE:
+			filaTemporal = filaTemporal + pasos;
+
+			break;
+		case NORESTE:
+			columnaTemporal += pasos;
+			filaTemporal += pasos;
+
+			break;
+		case ESTE:
+			columnaTemporal += pasos;
+
+			break;
+		case SURESTE:
+			columnaTemporal += pasos;
+			filaTemporal -= pasos;
+
+			break;
+		case SUR:
+			filaTemporal -= pasos;
+
+			break;
+		case SUROESTE:
+			columnaTemporal -= pasos;
+			filaTemporal -= pasos;
+
+			break;
+		case OESTE:
+			columnaTemporal -= pasos;
+
+			break;
+		case NOROESTE:
+			columnaTemporal -= pasos;
+			filaTemporal += pasos;
+
+			break;
+		}
+		if (filaTemporal >= 1 && filaTemporal <= 8 && columnaTemporal >= 'a' && columnaTemporal <= 'h') {
+			posicionTemporal = new Posicion(filaTemporal, columnaTemporal);
+			this.setPosicion(posicionTemporal);
+		} else {
+			throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+
+		}
+
+	}
 }
